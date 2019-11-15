ee385d3dbd989dc2341dc648a9154b4fe252a0c2
Davidsito <drrhenal@gmail.com>
2019-11-16 00:13:32 +0100

    Mensaje antes del merge

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,2 +1,3 @@
 ######## Primer ejemplo python########
 ########Segundo mensaje##################
+########Vamos a escribir este mensaje para hacer el merge en los dos scripts!!!!!!Mensaje2000
--- Ejemplo2.py
+++ Ejemplo2.py
@@ -1,3 +1,4 @@
 ##############  Este es un tercer mensaje de prueba########
 ########Este es un cuarto mensaje de prueba
 #####este es un quinto mensaje
+########Vamos a escribir este mensaje para hacer el merge en los dos scripts!!!!!!Mensaje1000
